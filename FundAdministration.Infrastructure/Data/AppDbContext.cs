1d5185344767d7de718acaef386bd21cffa232c6
makv1975@hotmail.com <makv1975@hotmail.com>
2025-10-19 10:47:55 +0100

    removed unwanted logic

--- FundAdministration.Infrastructure/Data/AppDbContext.cs
+++ FundAdministration.Infrastructure/Data/AppDbContext.cs
@@ -1,6 +1,4 @@
-﻿using Ardalis.SharedKernel;
-using FundAdministration.Core.Base;
-using FundAdministration.Core.Funds;
+﻿using FundAdministration.Core.Funds;
 using FundAdministration.Core.Investors;
 using FundAdministration.Core.Transactions;
 using Microsoft.EntityFrameworkCore;
@@ -9,7 +7,6 @@ using System.Reflection;
 namespace FundAdministration.Infrastructure.Data;
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
-    //private readonly IDomainEventDispatcher? _dispatcher = dispatcher;
     public DbSet<Fund> Funds => Set<Fund>();
     public DbSet<Investor> Investors => Set<Investor>();
     public DbSet<Transaction> Transactions => Set<Transaction>();
@@ -19,22 +16,4 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
     modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
   }
 
-  //public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
-  //{
-  //  int result = await base.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
-
-  //  if (_dispatcher == null) return result;
-
-  //  var entitiesWithEvents = ChangeTracker.Entries<EntityBase>()
-  //      .Select(e => e.Entity)
-  //      .Where(e => e.DomainEvents.Any())
-  //      .ToArray();
-
-  //  await _dispatcher.DispatchAndClearEvents(entitiesWithEvents);
-
-  //  return result;
-  //}
-
-  //public override int SaveChanges() =>
-  //      SaveChangesAsync().GetAwaiter().GetResult();
 }
